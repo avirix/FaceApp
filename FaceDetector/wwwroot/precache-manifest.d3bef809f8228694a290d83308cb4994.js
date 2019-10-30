9eba8bf0e85d08a18b9a8b29cc1e67ff18c1d2d4
Alexander <sania.sviridenko@gmail.com>
2019-10-30 19:12:54 +0200

    update project build dependencies

---
+++ FaceDetector/wwwroot/precache-manifest.d3bef809f8228694a290d83308cb4994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d4aa8cf74af78935c1b7cab3c391df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0822a6a0339366efe1b",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "9454f543cd4a5f306262",
+    "url": "/static/css/main.491bee12.chunk.css"
+  },
+  {
+    "revision": "a0822a6a0339366efe1b",
+    "url": "/static/js/2.86c7f83c.chunk.js"
+  },
+  {
+    "revision": "9454f543cd4a5f306262",
+    "url": "/static/js/main.9d5a6077.chunk.js"
+  },
+  {
+    "revision": "dd04a01b022eda68b9fd",
+    "url": "/static/js/runtime-main.c329a74b.js"
+  }
+]);
